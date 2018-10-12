06ac48a26a11328f283b10158597d4cf693280e2
johnomernik <john@omernik.com>
2018-10-12 15:47:01 -0500

    Updated start script to work from any dir

--- code/rtsp2stream.sh
+++ code/rtsp2stream.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-MYDIR=$(pwd)
-
-
-
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $MYDIR
 export LD_LIBRARY_PATH=/opt/mapr/lib
-LD_PRELOAD=/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server/libjvm.so $MYDIR/rtsp2stream.py
+LD_PRELOAD=/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server/libjvm.so ./rtsp2stream.py
